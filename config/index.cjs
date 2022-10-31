ac90692bc9ff8265fe5f625348d6c5c103d9265d
FishZodiac <leesummer117@gmail.com>
2022-10-31 10:54:52 +0800

    更新配置

--- config/index.cjs
+++ config/index.cjs
@@ -6,61 +6,24 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
-  ],
-
+  "APP_ID":"wxf53b5c107e31858b",
+  "APP_SECRET":"8aab81b0642028cbe1d716f0dec95703",
+  "IS_SHOW_COLOR":true,
+  "CALLBACK_TEMPLATE_ID":"",
+  "CALLBACK_USERS":[{"name":"自己","id":""}],
+  "USERS":[
+    {"name":"老婆","id":"o25ud53pl7mfwAIxGPpPUxyI43jg",
+     "useTemplateId":"RpLvqYz7QWv7q72f5SN8GdPyv1kI7gCCJmEJKUku1ZY",
+     "province":"重庆","city":"渝北区","horoscopeDate":"10-05",
+     "horoscopeDateType":"今日",
+     "openUrl":"https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_aladin_banner&city=%E9%87%8D%E5%BA%86-%E9%87%8D%E5%BA%86",
+     "festivals":[{"type":"节日","name":"结婚纪念日","date":"09-09","year":"2022"}],
+     "customizedDateList":[]}],
+  "SWITCH":{
+    "weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,
+    "earthyLoveWords":false,"momentCopyrighting":false,
+    "poisonChickenSoup":false,"poetry":false,"horoscope":false,
+    "birthdayMessage":true}
 }
 
 module.exports = USER_CONFIG
