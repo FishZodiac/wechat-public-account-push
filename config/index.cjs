535f855b3ecf392975a31f5ae64f36a3436c47c6
FishZodiac <leesummer117@gmail.com>
2024-08-16 09:36:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,14 +6,14 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  "APP_ID":"wxf53b5c107e31858b",
-  "APP_SECRET":"8aab81b0642028cbe1d716f0dec95703",
+  "APP_ID":"wx64923f60b2d079bc",
+  "APP_SECRET":"df0994ea806fb8a77bcc00a1ad866b66",
   "IS_SHOW_COLOR":true,
   "CALLBACK_TEMPLATE_ID":"",
   "CALLBACK_USERS":[{"name":"自己","id":""}],
   "USERS":[
     {"name":"亲亲老婆","id":"o25ud5-Oq77mNo58CNpSxrI4Osz4",
-     "useTemplateId":"RpLvqYz7QWv7q72f5SN8GdPyv1kI7gCCJmEJKUku1ZY",
+     "useTemplateId":"8bIDZYd-vFH8TcTsjAzt-c4ZWL_zyabZETC4BXjjqa0",
      "province":"重庆","city":"渝北区","horoscopeDate":"10-05",
      "horoscopeDateType":"今日",
      "openUrl":"https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_aladin_banner&city=%E9%87%8D%E5%BA%86-%E9%87%8D%E5%BA%86",
@@ -22,8 +22,8 @@ const USER_CONFIG = {
        {"keyword":"love_day",date:"2021-11-21"},
        {"keyword":"marry_day",date:"2022-09-09"}
      ]},
-     {"name":"老公","id":"o25ud53pl7mfwAIxGPpPUxyI43jg",
-     "useTemplateId":"RpLvqYz7QWv7q72f5SN8GdPyv1kI7gCCJmEJKUku1ZY",
+     {"name":"老公","id":"on8Z06Tf5pTuGEssuZ-k57d5-1V8",
+     "useTemplateId":"8bIDZYd-vFH8TcTsjAzt-c4ZWL_zyabZETC4BXjjqa0",
      "province":"重庆","city":"渝北区","horoscopeDate":"10-05",
      "horoscopeDateType":"今日",
      "openUrl":"https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_aladin_banner&city=%E9%87%8D%E5%BA%86-%E9%87%8D%E5%BA%86",
@@ -33,7 +33,7 @@ const USER_CONFIG = {
        {"keyword":"marry_day",date:"2022-09-09"}
      ]}],
   "SWITCH":{
-    "weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,
+    "weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,
     "earthyLoveWords":false,"momentCopyrighting":false,
     "poisonChickenSoup":false,"poetry":false,"horoscope":false,
     "birthdayMessage":true}
